998a070dfc5b67b4f806f482e3c78f602ffb1984
JulianDunsbergen <Robotninja100@gmail.com>
2025-11-20 21:13:09 +0100

    fix wit scherm

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sprint-united/',
+  base: '/Sprin_united.github.co/',
 })
