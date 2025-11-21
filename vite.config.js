c8ce4706386c052c189b0bf84ef1a7ecfcb966bd
JulianDunsbergen <Robotninja100@gmail.com>
2025-11-21 08:58:21 +0100

    router basename toegevoegd

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sprin_united.github.io/',
+  base: '/Sprint_united/',
 })
