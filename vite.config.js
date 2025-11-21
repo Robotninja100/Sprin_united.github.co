af6e3c45f1ec70433dded2f4562c925898f9b99d
JulianDunsbergen <Robotninja100@gmail.com>
2025-11-21 08:36:23 +0100

    config update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/Sprin_united.github.io/',
-=======
-  base: '/Sprin_united.github.co/',
->>>>>>> d6c2e5c9326408cabce6410b386236a79ca4d0b6
 })
